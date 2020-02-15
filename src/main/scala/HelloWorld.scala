0a5bedfd8b237a367c8725ade19ea9a356dfcfa2
alessandro <alelombo55@gmail.com>
2020-02-15 16:22:37 +0100

    Update previous commit.

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,3 +1,14 @@
-class HelloWorld {
+import org.apache.spark.sql.SparkSession
 
+object HelloWorld {
+  def main(args: Array[String]) {
+    println("Program starts")
+    val spark: SparkSession = SparkSession.builder().master("local[*]")
+      .appName("HelloWorld")
+      .getOrCreate()
+    val textFile = spark.read.textFile("README.md")
+    println("Lines: " + textFile.count())
+    spark.close()
+    println("Program ends")
+  }
 }
