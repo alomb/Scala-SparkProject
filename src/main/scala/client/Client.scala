f5841f57f09c5379d5de44eb88c4e00e003efee7
alessandro <alelombo55@gmail.com>
2020-04-09 10:20:44 +0200

    Improve security on the graph loader. Add some scaladoc.

--- src/main/scala/client/Client.scala
+++ src/main/scala/client/Client.scala
@@ -1,14 +1,16 @@
 package client
 
-import akka.actor.{ActorSystem, Props}
+import akka.actor.{ActorRef, ActorSystem, Props}
 import client.actors.MasterGraph
 import client.actors.MasterGraph.Start
 
 object Client {
   def main(args: Array[String]): Unit = {
-    val system: ActorSystem = ActorSystem("Test")
+    val system: ActorSystem = ActorSystem("Client")
 
-    val master = system.actorOf(Props(new MasterGraph(8827609, 20)), name = "Master")
+    val master: ActorRef = system.actorOf(Props(new MasterGraph(8227609, 20)),
+      name = "Master")
+
     master ! Start(0, 0)
   }
 }
--- src/main/scala/graph/GraphUtils.scala
+++ src/main/scala/graph/GraphUtils.scala
@@ -1,25 +1,48 @@
 package graph
 
+import java.nio.file.{Files, Paths}
+
+import client.writer.{EdgeFileFormat, VerticeFileFormat}
 import org.apache.spark.graphx.{Edge, Graph, VertexId}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
+/***
+ * Collects some utility functions
+ * @param spark The spark session used to parallelize in the cluster the below operations
+ */
 class GraphUtils(spark: SparkSession) {
   import spark.implicits._
   private val sparkContext = spark.sparkContext
   sparkContext.setLogLevel("ERROR")
 
-  def createGraphFromObs(filesNode: Seq[String], filesEdge: Seq[String]): Graph[String, Long] = {
-    // TODO Check presence of files
+  /***
+   * Create a [[Graph]] from multiple files containing raw observations
+   * @param filesVertice collection of csv file paths containing addresses and associated information
+   * @param filesEdge collection of csv file paths containing transactions between addresses and associated information
+   * @return a [[Graph]] from the data in the files. If the files are empty, or don't exist or have different quantities
+   *         an empty graph is returned.
+   */
+  def createGraphFromObs(filesVertice: Seq[String], filesEdge: Seq[String]): Graph[String, Long] = {
+    val existingVerticesPaths: Seq[String] = filesVertice.filter(f => Files.exists(Paths.get(f)))
+    val existingEdgesPaths: Seq[String] = filesEdge.filter(f => Files.exists(Paths.get(f)))
+
+    if(existingVerticesPaths.isEmpty ||
+      existingEdgesPaths.isEmpty ||
+      existingEdgesPaths.size != existingVerticesPaths.size) {
+
+      return Graph(spark.sparkContext.emptyRDD, spark.sparkContext.emptyRDD)
+    }
 
     val mapNodesIndex: collection.Map[String, VertexId] = spark.read
       .format("csv")
       .option("inferSchema", "true")
       .option("header", "true")
-      .load(filesNode:_*)
-      .as[String]
+      .load(existingVerticesPaths:_*)
+      .as[VerticeFileFormat]
       .rdd
       .mapPartitions(x => x.toList.distinct.toIterator)
+      .map{case VerticeFileFormat(s) => s}
       .zipWithIndex()
       .collectAsMap()
 
@@ -27,11 +50,11 @@ class GraphUtils(spark: SparkSession) {
       .format("csv")
       .option("inferSchema", "true")
       .option("header", "true")
-      .load(filesEdge:_*)
-      .as[(String, String, String, Long)]
+      .load(existingEdgesPaths:_*)
+      .as[EdgeFileFormat]
       .rdd
       .mapPartitions(x => x.toList.distinct.toIterator)
-      .map{case (_, in, out, v) => Edge(mapNodesIndex(in), mapNodesIndex(out), v)}
+      .map{case EdgeFileFormat(_, in, out, v) => Edge(mapNodesIndex(in), mapNodesIndex(out), v)}
 
     val nodes: RDD[(VertexId, String)] = sparkContext.parallelize(mapNodesIndex.map(v => (v._2, v._1)).toSeq)
 
